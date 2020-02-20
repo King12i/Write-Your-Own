c5b80fbc6ad1a2b075b720cbd1f66c4abf61376b
Lutteur Ngengwa <ngengwalutteur11@gmail.com>
2020-02-20 09:32:47 -0800

    Write Your Own

---
+++ Write_Your_Own.js
@@ -0,0 +1,34 @@
+var name = "Bob";
+var isRaining = true;
+var hasPet = false;
+var job = "Web Developer";
+var cash = 100;
+if (isRaining == isRaining){
+    console.log( "I need a coat"  )
+}
+cash-= 50;
+isRaining = false;
+if (name == false){
+    console.log( "Hello stranger")
+}
+else if (job == "Web Developer"){
+    console.log("Keep up the good work")
+}
+else {
+    console("Hello Bob")
+}
+cash = cash + 500;
+job =  "Senior Web Developer";
+hasPet =true;
+cash = cash - 1500;
+if (cash > 1000){
+    console.log( "Vacation time!");
+}
+cash = cash - 2000;
+console.log( "Payday!");
+cash = cash +2000;
+console.log( name);
+console.log( cash);
+console.log( job);
+console.log( isRaining);
+console.log( hasPet);
